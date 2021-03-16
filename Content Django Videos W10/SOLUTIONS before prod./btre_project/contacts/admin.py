9f7002534f11c646dc4e64d63ee2540544cb24ef
daniel.ach <MrAch26@users.noreply.github.com>
2021-03-16 11:03:38 +0200

    End of the project before solutions

---
+++ Content Django Videos W10/SOLUTIONS before prod./btre_project/contacts/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Contact
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
+    list_per_page = 25
+
+# admin.site.register(Contact, ContactAdmin)
+
+
+# TODO: To register all Apps FIXME:
+# from django.apps import apps
+
+# models = apps.get_models()
+# for model in models:
+#     try:
+#         admin.site.register(model)
+#     except admin.sites.AlreadyRegistered:
+#         pass
